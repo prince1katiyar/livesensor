0b50619cb8d5f1e05464b7eb3dd656fd9fde444e
prince1katiyar <princekatiyar986@gmail.com>
2024-04-09 14:11:37 +0530

    first

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages ,setup 
+#from typing import List
+#from typing import List
+
+
+
+
+
+setup (
+name = 'sensor',
+version="0.0.1",
+author="prince",
+author_email="princekatiyar986@gmail.com",
+packages = find_packages(),
+install_requires = ["pymongo"]
+
+)
+
